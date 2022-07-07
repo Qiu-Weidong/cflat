725d38a9933b0f14f0d4b655bb12c58b1de59a22
Qiu-Weidong <568385476@qq.com>
2022-07-07 11:49:20 +0800

    .

---
+++ src/compiler/DeclarationResolver.h
@@ -0,0 +1,11 @@
+#ifndef CFLAT_COMPILER_DECLARATIONRESOLVER_H_
+#define CFLAT_COMPILER_DECLARATIONRESOLVER_H_
+#include "CflatBaseVisitor.h"
+
+
+class DeclarationResolver : public CflatBaseVisitor {
+    
+}; 
+
+#endif // CFLAT_COMPILER_DECLARATIONRESOLVER_H_
+
